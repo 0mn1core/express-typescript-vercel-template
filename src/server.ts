73f34878d0b32fabd17e993009ced4ff04a669df
0mn1core <omn1coreprocessing@gmail.com>
2024-08-23 13:33:33 -0400

    Add KV integration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express, { type Express } from "express";
+// Set up express async error handling
+import "express-async-errors"
 import cors from "cors";
+
+// Router imports
+import kvRouterCreator from "./routers/kv";
 
 const app: Express = express();
 
@@ -10,5 +15,8 @@ app.use(cors());
 app.get('/', (_req, res) =>{
   res.send("Express Typescript Server");
 });
+
+// Routers
+app.use("/kv", kvRouterCreator(app));
 
 export { app }
